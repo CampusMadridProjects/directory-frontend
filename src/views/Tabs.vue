fe570b86fb6c573e89ea2dc72284da01c962690a
ivoriginal <ivo.vilches@gmail.com>
2018-08-07 18:05:14 +0200

    title caps changed

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -90,7 +90,7 @@ export default {
   data () {
     return {
       fixed: false,
-      title: 'Campus directory',
+      title: 'Campus Directory',
       searching: false,
       tabs: null,
       search: '',
