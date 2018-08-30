8f3ec5d7fe8abe86acc83e2b896f6bb157ea21db
ivoriginal <ivo.vilches@gmail.com>
2018-08-30 20:38:35 +0200

    test

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,11 +5,11 @@ import 'vuetify/dist/vuetify.min.css'
 Vue.use(Vuetify, {
   theme: {
     primary: '#ee44aa',
-    secondary: '#424242',
-    accent: '#82B1FF',
-    error: '#FF5252',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FFC107'
+    secondary: '#ee44aa',
+    accent: '#ee44aa',
+    error: '#ee44aa',
+    info: '#ee44aa',
+    success: '#ee44aa',
+    warning: '#ee44aa'
   },
 })
