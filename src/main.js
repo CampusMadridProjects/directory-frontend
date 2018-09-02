87f31a7a20d3f434ee426db7324712c86757d80a
Carlos Hernandez <carloshdmn@gmail.com>
2018-09-02 17:32:53 +0200

    Location request tracking

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import '@babel/polyfill';
 import Vue from 'vue';
+import VueAnalytics from 'vue-analytics';
 import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
-import VueAnalytics from 'vue-analytics'
+
+console.log(process.env);
 
 Vue.use(VueAnalytics, {
-  id: process.env.ANALYTICS_TRACKING_ID,
+  id: process.env.VUE_APP_ANALYTICS_TRACKING_ID,
   router,
 });
 
