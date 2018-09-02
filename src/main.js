1d36c3654cdb79a5faa6bc6018a1ae18c3833953
Carlos Hernandez <carloshdmn@gmail.com>
2018-09-02 14:09:46 +0200

    Add ga tracking

--- src/main.js
+++ src/main.js
@@ -4,6 +4,17 @@ import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import './registerServiceWorker';
+import VueAnalytics from 'vue-analytics'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-125086753-1',
+  router,
+  // autoTracking: {
+  //   screenview: true
+  // }
+});
+
+console.log('v5');
 
 Vue.config.productionTip = false;
 
