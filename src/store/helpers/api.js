5b28cf463fbe7d5c5fbc4ecf410a10b9a1bfe0b7
Carlos Hernandez <carloshdmn@gmail.com>
2019-08-09 10:11:03 +0200

    Set self location API

--- src/store/helpers/api.js
+++ src/store/helpers/api.js
@@ -1,9 +1,5 @@
 function getUrl() {
-  const protocol = process.env.VUE_APP_API_PROTOCOL;
-  const url = process.env.VUE_APP_API_URL;
-  const subdomain = '';
-
-  return protocol + subdomain + url;
+  return window.location.origin + '/api'
 }
 
 export default {
