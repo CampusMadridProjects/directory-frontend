03e83d6e479ae8c81ba6582c7156f3c3fa080e7c
Carlos Hernandez <carloshdmn@gmail.com>
2019-09-10 20:19:35 +0200

    Improve API change mode

--- src/store/helpers/api.js
+++ src/store/helpers/api.js
@@ -1,5 +1,11 @@
 function getUrl() {
-  return `${window.location.origin}/api`;
+  let api = `${window.location.origin}/api`;
+  
+  if (process.env.VUE_APP_API_URL) {
+  	api = process.env.VUE_APP_API_URL;
+  }
+
+  return api;
 }
 
 export default {
