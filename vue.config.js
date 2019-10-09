546dc746c2a2ee2d9fe672548c1ed03442ea7b1d
Carlos Hernandez <carloshdmn@gmail.com>
2019-10-09 15:32:47 +0200

    enable cache invalidation policies

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+const manifestJSON = require('./public/manifest.json');
+
+module.exports = {
+  publicPath: './',
+  pwa: {
+    themeColor: manifestJSON.theme_color,
+    name: manifestJSON.short_name,
+    msTileColor: manifestJSON.background_color,
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'src/service-worker.js',
+    },
+  },
+};
