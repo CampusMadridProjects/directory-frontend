eb6e0e16d3c4647b9c8da9e8f4fad98d436408d7
ivoriginal <ivo.vilches@gmail.com>
2020-01-24 20:56:41 +0100

    updated profile styles

--- src/views/PersonDetail.vue
+++ src/views/PersonDetail.vue
@@ -65,25 +65,45 @@
         <!-- /Profile info container -->
       </div>
       <!-- /User picture, name, job title and bio -->
-
+      <!-- Location -->
+      <v-card-title primary-title>
+        <!-- User info -->
+        <div class="location" v-if="data.location">
+          <v-icon size="14" class="mr-1">room</v-icon>
+          <span>{{ data.location }}</span>
+        </div>
+        <!-- /User info -->
+      </v-card-title>
+      <!-- /Location -->
+      <!-- Knows about -->
       <v-card-title primary-title>
         <!-- User info -->
         <div class="card-user-info">
           <!-- Tags and location -->
+          <span>Knows about</span>
           <h4>
-            <v-icon size="14" class="mr-1">forum</v-icon>
             <span v-for="(ability, index) in data.Tag" :key="ability.id">{{ (index !== 0) ? ', ' + ability.name : ability.name }}</span>
           </h4>
-          <v-icon size="14" class="mr-1">room</v-icon>
-          <span>{{ data.location }}</span>
           <!-- Tags and location -->
         </div>
         <!-- /User info -->
       </v-card-title>
-      
+      <!-- /Knows about -->
+      <!-- Needs help with -->
+      <v-card-title primary-title>
+        <!-- User info -->
+        <div class="card-user-info">
+          <span>Needs help with</span>
+          <h4>
+            <span v-for="(ability, index) in data.Tag" :key="ability.id">{{ (index !== 0) ? ', ' + ability.name : ability.name }}</span>
+          </h4>
+          <!-- Tags and location -->
+        </div>
+        <!-- /User info -->
+      </v-card-title>
+      <!-- /Needs help with -->
       <!-- Connect options -->
-      <v-card-text>
-        <div class="mb-3">
+      <div class="px-3 my-2 bottom-cta">
           <v-btn color="primary" x-large v-if="connect.show"
             :href="connect.url"
             target="_blank"
@@ -114,7 +134,8 @@
             Connect
           </v-btn> -->
         </div>
-
+      <div class="px-4 my-3">
+        <h4>Social profiles</h4>
         <div class="person-card-social-icons">
           <a v-if="data.instagram"
             :href="data.instagram"
@@ -123,6 +144,7 @@
             @click="$ga.event('person_detail', 'instagram', data._id)"
           >
             <img src="img/instagram_64.png" alt="instagram" />
+            <span>Instagram</span>
           </a>
           <a v-if="data.twitter"
             :href="data.twitter"
@@ -131,6 +153,7 @@
             @click="$ga.event('person_detail', 'twitter', data._id)"
           >
             <img src="img/twitter_64.png" alt="twitter" />
+            <span>Twitter</span>
           </a>
           <a v-if="data.linkedin"
             :href="data.linkedin"
@@ -139,6 +162,7 @@
             @click="$ga.event('person_detail', 'linkedin', data._id)"
           >
             <img src="img/linkedin_64.png" alt="linkedin" />
+            <span>LinkedIn</span>
           </a>
           <a v-if="data.slack"
             :href="slackUrl(data.slack)"
@@ -147,9 +171,10 @@
             @click="$ga.event('person_detail', 'slack', data._id)"
           >
             <img src="img/slack_64.png" alt="slack" />
+            <span>Slack</span>
           </a>
         </div>
-      </v-card-text>
+      </div>
       <!-- /Connect options -->
     </div>
 
@@ -158,16 +183,106 @@
 </template>
 
 <style>
+  /* */
   .v-btn {
     text-transform: none;
   }
+  /* adds space between job title and bio */
   .job {
     display: block;
     margin-bottom: 8px;
   }
+  /* fixes connect bottom position */
+  .bottom-cta {
+    bottom: 0px;
+    position: fixed;
+    width: 360px;
+  }
+  .v-toolbar__content {
+    padding: 0 16px;
+  }
+  /* removes underline from links */
+  a  {
+    text-decoration: none;
+  }
+  
+  /*  */
+  .person-card-social-icons {
+    display: flex;
+    flex-direction: column-reverse;
+    justify-content: space-between;
+  }
+  
+  .person-card-social-icon {
+    display: inline-block;
+    padding: 0 8px;
+    display: flex;
+    align-content: center;
+    margin: 4px 0px;
+    background: #fafafa;
+    padding: 8px;
+    color: black;
+    align-items: center;
+    border-radius: 4px;
+  }
+
+  .person-card-social-icon img {
+    height: 32px;
+    width: 32px;
+    margin-right: 8px;
+    border-radius: 50px;
+  }
+
+
+  @media screen and (max-width: 768px) {
+    .bottom-cta {
+      width: 100%;
+    }
+    .v-toolbar {
+      width: 100% !important;
+    }
+    /* makes profile CTA bigger */
+    .bottom-cta .v-btn {
+      height: 56px;
+      font-size: 1.4rem;
+    }
+    /* */
+    .headline {
+      font-size: 2rem !important;
+    }
+    .job {
+      font-size: 1.4rem;
+    }
+  }
 </style>
 
 <style scoped>
+  
+  .v-toolbar {
+    position: fixed;
+    width: 360px;
+  }
+  
+  .full-size {
+    padding-bottom: 20%;
+  }
+  
+  .v-card__title--primary {
+    padding: 4px 24px;
+  }
+  
+  .location {
+    margin-bottom: 8px;
+  }
+  
+  .card-user-info {
+    text-align: left;
+    width: 100%;
+    background: #fafafa;
+    padding: 8px;
+    border-radius: 4px;
+  }
+  
   .card-user-pic {
     height: calc(43vw - 64px);
     margin-bottom: 0px;
@@ -214,7 +329,7 @@
   }
 
   .headline {
-    margin-top: 12px;
+    margin-top: 8px;
   }
 
   .v-btn--icon {
@@ -244,7 +359,7 @@
     }
 
     .card-user-pic {
-      height: calc(66vh - 64px);
+      height: calc(56vh - 64px);
     }
   }
 </style>
