5a28eebccaa1e79839232f75da7ab79ea01b0b33
ivoriginal <ivo.vilches@gmail.com>
2020-01-27 18:26:32 +0100

    less fake content

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,182 +1,128 @@
 <template>
-  <div
-    id="e3"
-    style="max-width: 400px; margin: auto;"
-    class="grey lighten-3"
-  >
-    <v-card>
-      <v-container
-        fluid
-        grid-list-lg
-      >
-        <v-layout row wrap>
-          <v-flex xs12>
-            <v-card color="blue-grey darken-2" class="white--text">
-              <v-card-title primary-title>
-                <div>
-                  <div class="headline">Unlimited music now</div>
-                  <span>Listen to your favorite artists and albums whenever and wherever, online and offline.</span>
-                </div>
-              </v-card-title>
-              <v-card-actions>
-                <v-btn flat dark>Listen now</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
+  <div>
+    <v-container
+      fluid
+      grid-list-lg
+    >
+      <v-layout row wrap>
+        <v-flex xs12 sm6 offset-sm3>
+          <strong class="title">
+          Featured news
+          </strong>
+          <v-card>
+            <v-img src="https://www.elreferente.es/source/bnext.jpg"
+              aspect-ratio="1.9"
+            ></v-img>
 
-          <v-flex xs12>
-            <v-card color="cyan darken-2" class="white--text">
-              <v-layout>
-                <v-flex xs5>
-                  <v-img
-                    src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"
-                    height="125px"
-                    contain
-                  ></v-img>
-                </v-flex>
-                <v-flex xs7>
-                  <v-card-title primary-title>
-                    <div>
-                      <div class="headline">Supermodel</div>
-                      <div>Foster the People</div>
-                      <div>(2014)</div>
-                    </div>
-                  </v-card-title>
-                </v-flex>
-              </v-layout>
-              <v-divider light></v-divider>
-              <v-card-actions class="pa-3">
-                Rate this album
-                <v-spacer></v-spacer>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">Bnext levanta 22 millones</h3>
+                <div> Vivan los neobancos </div>
+              </div>
+            </v-card-title>
 
-          <v-flex xs12>
-            <v-card color="purple" class="white--text">
-              <v-layout row>
-                <v-flex xs7>
-                  <v-card-title primary-title>
-                    <div>
-                      <div class="headline">Halycon Days</div>
-                      <div>Ellie Goulding</div>
-                      <div>(2013)</div>
-                    </div>
-                  </v-card-title>
-                </v-flex>
-                <v-flex xs5>
-                  <v-img
-                    src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
-                    height="125px"
-                    contain
-                  ></v-img>
-                </v-flex>
-              </v-layout>
-              <v-divider light></v-divider>
-              <v-card-actions class="pa-3">
-                Rate this album
-                <v-spacer></v-spacer>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-          
-          <v-flex xs12>
-            <v-card color="blue-grey darken-2" class="white--text">
-              <v-card-title primary-title>
-                <div>
-                  <div class="headline">Unlimited music now</div>
-                  <span>Listen to your favorite artists and albums whenever and wherever, online and offline.</span>
-                </div>
-              </v-card-title>
-              <v-card-actions>
-                <v-btn flat dark>Listen now</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
+            <v-card-actions>
+              <v-btn flat color="blue">Share</v-btn>
+              <v-btn flat color="blue">Explore</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
 
-          <v-flex xs12>
-            <v-card color="cyan darken-2" class="white--text">
-              <v-layout>
-                <v-flex xs5>
-                  <v-img
-                    src="https://cdn.vuetifyjs.com/images/cards/foster.jpg"
-                    height="125px"
-                    contain
-                  ></v-img>
-                </v-flex>
-                <v-flex xs7>
-                  <v-card-title primary-title>
-                    <div>
-                      <div class="headline">Supermodel</div>
-                      <div>Foster the People</div>
-                      <div>(2014)</div>
-                    </div>
-                  </v-card-title>
-                </v-flex>
-              </v-layout>
-              <v-divider light></v-divider>
-              <v-card-actions class="pa-3">
-                Rate this album
-                <v-spacer></v-spacer>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
+        <v-flex xs12 sm6 offset-sm3>
+          <!-- <v-list-tile-content> -->
+          <v-list-tile-title>New members</v-list-tile-title>
+          <v-list-tile-sub-title class="text--primary">Go share some coffee</v-list-tile-sub-title>
+<!--
+          <v-btn icon>
+            <v-icon>arrow_right</v-icon>
+          </v-btn>
+-->
+          <!-- </v-list-tile-content> -->
 
-          <v-flex xs12>
-            <v-card color="purple" class="white--text">
-              <v-layout row>
-                <v-flex xs7>
-                  <v-card-title primary-title>
-                    <div>
-                      <div class="headline">Halycon Days</div>
-                      <div>Ellie Goulding</div>
-                      <div>(2013)</div>
-                    </div>
-                  </v-card-title>
-                </v-flex>
-                <v-flex xs5>
-                  <v-img
-                    src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
-                    height="125px"
-                    contain
-                  ></v-img>
-                </v-flex>
-              </v-layout>
-              <v-divider light></v-divider>
-              <v-card-actions class="pa-3">
-                Rate this album
-                <v-spacer></v-spacer>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-                <v-icon>star_border</v-icon>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
+          <div data-v-387ef93f="" class="flex card-grid-item xs12 sm6 md3 lg2 xl2"><div data-v-2b78fdfc="" data-v-387ef93f="" class="light-border custom-hover card-grid-item-card hidden-md-and-up v-card v-card--hover v-sheet theme--light"><div data-v-2b78fdfc="" role="listitem"><a class="v-list__tile v-list__tile--link theme--light"><div data-v-2b78fdfc=""><div data-v-2b78fdfc="" class="v-list__tile__avatar" style="background-image: url(&quot;https://storage.googleapis.com/gfs-directory/pic/J/6/J6dK_lLMf1UF-daHH54_9.png&quot;), url(&quot;/img/nopic.png&quot;);"><div data-v-2b78fdfc="" class="v-avatar" style="height: 40px; width: 40px;"></div></div></div><div data-v-2b78fdfc="" class="person-info"><div data-v-2b78fdfc="" class="v-list__tile__title text-xs-left">Jaime </div><div data-v-2b78fdfc="" class="v-list__tile__sub-title text-xs-left">
+        CEO
+        <span data-v-2b78fdfc="">@</span>
+          Vetnify S.L.
+        </div><div data-v-2b78fdfc="" class="card-user-info text-xs-left ellipsis"><h5 data-v-2b78fdfc="" class="ellipsis"><i data-v-2b78fdfc="" aria-hidden="true" class="v-icon mr-1 material-icons theme--light" style="font-size: 14px;">forum</i><span data-v-2b78fdfc="">Business</span><span data-v-2b78fdfc="">, Design</span><span data-v-2b78fdfc="">, Technology</span><span data-v-2b78fdfc="">, Marketing</span></h5></div></div></a></div></div><div data-v-d74d3096="" data-v-387ef93f="" class="box-shadow light-border custom-hover card-elements ellipsis card-grid-item-card hidden-sm-and-down v-card v-card--hover v-sheet theme--light"><div data-v-d74d3096=""><div data-v-d74d3096="" class="card-user-pic" style="background-image: url(&quot;https://storage.googleapis.com/gfs-directory/pic/J/6/J6dK_lLMf1UF-daHH54_9.png&quot;), url(&quot;/img/nopic.png&quot;);"></div><div data-v-d74d3096="" style="padding: 8px 24px;"><div data-v-d74d3096="" class="headline ellipsis">Jaime Frühbeck</div><div data-v-d74d3096="" class="ellipsis"><span data-v-d74d3096="" class="one-line">
+            CEO
+            <span data-v-d74d3096="">@</span>
+            Vetnify S.L.
+          </span></div></div></div><div data-v-d74d3096="" class="v-card__title v-card__title--primary"><div data-v-d74d3096="" class="card-user-info"><h4 data-v-d74d3096="" class="ellipsis"><i data-v-d74d3096="" aria-hidden="true" class="v-icon mr-1 material-icons theme--light" style="font-size: 14px;">forum</i><span data-v-d74d3096="">Business
+        </span><span data-v-d74d3096="">, Design
+        </span><span data-v-d74d3096="">, Technology
+        </span><span data-v-d74d3096="">, Marketing
+        </span></h4><div data-v-d74d3096=""><i data-v-d74d3096="" aria-hidden="true" class="v-icon mr-1 material-icons theme--light" style="font-size: 14px;">room</i><span data-v-d74d3096="">Community</span></div></div></div></div></div>
 
-        </v-layout>
-      </v-container>
-    </v-card>
+          <div data-v-387ef93f="" class="flex card-grid-item xs12 sm6 md3 lg2 xl2"><div data-v-2b78fdfc="" data-v-387ef93f="" class="light-border custom-hover card-grid-item-card hidden-md-and-up v-card v-card--hover v-sheet theme--light"><div data-v-2b78fdfc="" role="listitem"><a class="v-list__tile v-list__tile--link theme--light"><div data-v-2b78fdfc=""><div data-v-2b78fdfc="" class="v-list__tile__avatar" style="background-image: url(&quot;https://storage.googleapis.com/gfs-directory/pic/J/6/J6dK_lLMf1UF-daHH54_9.png&quot;), url(&quot;/img/nopic.png&quot;);"><div data-v-2b78fdfc="" class="v-avatar" style="height: 40px; width: 40px;"></div></div></div><div data-v-2b78fdfc="" class="person-info"><div data-v-2b78fdfc="" class="v-list__tile__title text-xs-left">Jaime </div><div data-v-2b78fdfc="" class="v-list__tile__sub-title text-xs-left">
+        CEO
+        <span data-v-2b78fdfc="">@</span>
+          Vetnify S.L.
+        </div><div data-v-2b78fdfc="" class="card-user-info text-xs-left ellipsis"><h5 data-v-2b78fdfc="" class="ellipsis"><i data-v-2b78fdfc="" aria-hidden="true" class="v-icon mr-1 material-icons theme--light" style="font-size: 14px;">forum</i><span data-v-2b78fdfc="">Business</span><span data-v-2b78fdfc="">, Design</span><span data-v-2b78fdfc="">, Technology</span><span data-v-2b78fdfc="">, Marketing</span></h5></div></div></a></div></div><div data-v-d74d3096="" data-v-387ef93f="" class="box-shadow light-border custom-hover card-elements ellipsis card-grid-item-card hidden-sm-and-down v-card v-card--hover v-sheet theme--light"><div data-v-d74d3096=""><div data-v-d74d3096="" class="card-user-pic" style="background-image: url(&quot;https://storage.googleapis.com/gfs-directory/pic/J/6/J6dK_lLMf1UF-daHH54_9.png&quot;), url(&quot;/img/nopic.png&quot;);"></div><div data-v-d74d3096="" style="padding: 8px 24px;"><div data-v-d74d3096="" class="headline ellipsis">Jaime Frühbeck</div><div data-v-d74d3096="" class="ellipsis"><span data-v-d74d3096="" class="one-line">
+            CEO
+            <span data-v-d74d3096="">@</span>
+            Vetnify S.L.
+          </span></div></div></div><div data-v-d74d3096="" class="v-card__title v-card__title--primary"><div data-v-d74d3096="" class="card-user-info"><h4 data-v-d74d3096="" class="ellipsis"><i data-v-d74d3096="" aria-hidden="true" class="v-icon mr-1 material-icons theme--light" style="font-size: 14px;">forum</i><span data-v-d74d3096="">Business
+        </span><span data-v-d74d3096="">, Design
+        </span><span data-v-d74d3096="">, Technology
+        </span><span data-v-d74d3096="">, Marketing
+        </span></h4><div data-v-d74d3096=""><i data-v-d74d3096="" aria-hidden="true" class="v-icon mr-1 material-icons theme--light" style="font-size: 14px;">room</i><span data-v-d74d3096="">Community</span></div></div></div></div></div>
+
+          <div data-v-387ef93f="" class="flex card-grid-item xs12 sm6 md3 lg2 xl2"><div data-v-2b78fdfc="" data-v-387ef93f="" class="light-border custom-hover card-grid-item-card hidden-md-and-up v-card v-card--hover v-sheet theme--light"><div data-v-2b78fdfc="" role="listitem"><a class="v-list__tile v-list__tile--link theme--light"><div data-v-2b78fdfc=""><div data-v-2b78fdfc="" class="v-list__tile__avatar" style="background-image: url(&quot;https://storage.googleapis.com/gfs-directory/pic/J/6/J6dK_lLMf1UF-daHH54_9.png&quot;), url(&quot;/img/nopic.png&quot;);"><div data-v-2b78fdfc="" class="v-avatar" style="height: 40px; width: 40px;"></div></div></div><div data-v-2b78fdfc="" class="person-info"><div data-v-2b78fdfc="" class="v-list__tile__title text-xs-left">Jaime </div><div data-v-2b78fdfc="" class="v-list__tile__sub-title text-xs-left">
+        CEO
+        <span data-v-2b78fdfc="">@</span>
+          Vetnify S.L.
+        </div><div data-v-2b78fdfc="" class="card-user-info text-xs-left ellipsis"><h5 data-v-2b78fdfc="" class="ellipsis"><i data-v-2b78fdfc="" aria-hidden="true" class="v-icon mr-1 material-icons theme--light" style="font-size: 14px;">forum</i><span data-v-2b78fdfc="">Business</span><span data-v-2b78fdfc="">, Design</span><span data-v-2b78fdfc="">, Technology</span><span data-v-2b78fdfc="">, Marketing</span></h5></div></div></a></div></div><div data-v-d74d3096="" data-v-387ef93f="" class="box-shadow light-border custom-hover card-elements ellipsis card-grid-item-card hidden-sm-and-down v-card v-card--hover v-sheet theme--light"><div data-v-d74d3096=""><div data-v-d74d3096="" class="card-user-pic" style="background-image: url(&quot;https://storage.googleapis.com/gfs-directory/pic/J/6/J6dK_lLMf1UF-daHH54_9.png&quot;), url(&quot;/img/nopic.png&quot;);"></div><div data-v-d74d3096="" style="padding: 8px 24px;"><div data-v-d74d3096="" class="headline ellipsis">Jaime Frühbeck</div><div data-v-d74d3096="" class="ellipsis"><span data-v-d74d3096="" class="one-line">
+            CEO
+            <span data-v-d74d3096="">@</span>
+            Vetnify S.L.
+          </span></div></div></div><div data-v-d74d3096="" class="v-card__title v-card__title--primary"><div data-v-d74d3096="" class="card-user-info"><h4 data-v-d74d3096="" class="ellipsis"><i data-v-d74d3096="" aria-hidden="true" class="v-icon mr-1 material-icons theme--light" style="font-size: 14px;">forum</i><span data-v-d74d3096="">Business
+        </span><span data-v-d74d3096="">, Design
+        </span><span data-v-d74d3096="">, Technology
+        </span><span data-v-d74d3096="">, Marketing
+        </span></h4><div data-v-d74d3096=""><i data-v-d74d3096="" aria-hidden="true" class="v-icon mr-1 material-icons theme--light" style="font-size: 14px;">room</i><span data-v-d74d3096="">Community</span></div></div></div></div></div>
+
+          <v-btn>See all</v-btn>
+        </v-flex>
+
+        <v-flex xs12 sm6 offset-sm3>
+          <strong class="title">
+          Creator Studio
+          </strong>
+          <v-card>
+            <v-img src="https://www.podiumpodcast.com/wp-content/uploads/2019/03/Vostok6-1200x630lettering.jpg"
+              aspect-ratio="1.9"
+            ></v-img>
+
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">Vostok 6</h3>
+                <div>El podcast de las pioneras</div>
+              </div>
+            </v-card-title>
+
+            <v-card-actions>
+              <v-btn flat color="blue">Share</v-btn>
+              <v-btn flat color="blue">Explore</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <style scoped>
+  
+  /* */
+  .title {
+    font-size: 2.4rem !important;
+  }
+  
+  /* */
+  .v-card {
+    border: 1px solid #eaeaea;
+  }
+  
 </style>
 
 <script>
