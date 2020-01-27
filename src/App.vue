4c3842726d0f718437cc437642a2822015e14c48
ivoriginal <ivo.vilches@gmail.com>
2020-01-27 14:39:11 +0100

    added global media queries
    pls review this @codingcarlos

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,19 @@
   html, body, .application {
     font-family: 'Google Sans';
   }
+  /* sets card container size for screen sizes between ipad and desktop */
+  @media only screen and (min-width: 960px) and (max-width: 1263px) {
+    .container {
+      max-width: 1124px;
+    }
+  }
+  /* removes card container max width in bigger screen sizes */
+  @media only screen and (min-width: 1440px) {
+    .container {
+      max-width: none !important;
+      padding: 24px 3%;
+    }
+  }
 </style>
 
 <script>
