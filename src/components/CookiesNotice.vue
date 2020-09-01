a26d2ce947866a619803eb912ebb9fabfc97a117
Ivo <ivo@commitsans.com>
2020-09-01 16:56:12 +0200

    cookies banner styles updated

--- src/components/CookiesNotice.vue
+++ src/components/CookiesNotice.vue
@@ -1,47 +1,39 @@
 <template>
   <div class="cookies-notice" v-if="!cookiesAccepted">
-    <div>
-      Hi! To discover how to make a better product for you, we use third party cookies.
-      <a class="cookies-read" @click="$emit('read')">
-        Read cookie policy
-      </a>.
+    <div class="mr-1">
+      To discover how to make a better product for you, we use third party cookies.
+      <a class="cookies-read" @click="$emit('read')">Read cookie policy</a>.
     </div>
 
-    <v-btn @click="acceptCookies()">
-      <span data-t>It's ok</span>
+    <v-btn depressed color="white" @click="acceptCookies()">
+      <span data-t>Accept</span>
     </v-btn>
   </div>
 </template>
 
 <style scoped>
-.cookies-notice {
-  align-items: center;
-  background: #4285f4;
-  bottom: 0px;
-  color: #ffffff;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  left: 0px;
-  padding: 24px;
-  position: fixed;
-  width: 100%;
-  z-index: 999;
-}
-.gfs .cookies-notice {
-  font-family: 'Google Sans';
-}
-
+  .cookies-notice {
+    align-items: center;
+    background: #4285f4;
+    bottom: 0px;
+    color: #ffffff;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    left: 0px;
+    padding: 24px;
+    position: fixed;
+    width: 100%;
+    z-index: 999;
+    font-size: 1.2rem;
+  }
+  .gfs .cookies-notice {
+    font-family: 'Google Sans';
+  }
   .cookies-notice .cookies-read {
     text-decoration: underline;
     color: #fff;
   }
-
-@media screen and (max-width: 960px) {
-  .cookies-notice {
-    bottom: 56px;
-  }
-}
 </style>
 
 <script>
