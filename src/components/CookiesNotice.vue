f3e996815e375d6d5796f2a3ff8c00b3c480bc80
Ivo <ivo@commitsans.com>
2021-02-16 17:37:45 +0100

    minor style fixes

--- src/components/CookiesNotice.vue
+++ src/components/CookiesNotice.vue
@@ -1,32 +1,34 @@
 <template>
-  <div class="cookies-notice" v-if="!cookiesAccepted">
+  <div class="cookies-notice custom-elevation" v-if="!cookiesAccepted">
     <div class="mr-1">
       To discover how to make a better product for you, we use third party cookies.
       <a class="cookies-read" @click="$emit('read')">Read cookie policy</a>.
     </div>
 
-    <v-btn depressed color="white" @click="acceptCookies()">
+    <v-btn depressed color="white" @click="acceptCookies()" class="ml-4">
       <span data-t>Accept</span>
     </v-btn>
   </div>
 </template>
 
 <style scoped>
+
   .cookies-notice {
     align-items: center;
     background: #4285f4;
-    bottom: 0px;
+    bottom: 16px;
     color: #ffffff;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
-    left: 0px;
     padding: 24px;
     position: fixed;
-    width: 100%;
     z-index: 999;
     font-size: 1.2rem;
+    border-radius: 4px;
+    align-self: center;
   }
+
   .gfs .cookies-notice {
     font-family: 'Google Sans';
   }
@@ -34,6 +36,24 @@
     text-decoration: underline;
     color: #fff;
   }
+
+  @media only screen and (max-width: 900px) {
+    .cookies-notice {
+      flex-direction: column;
+      margin: 16px;
+      left: 0px;
+      bottom: 0px;
+    }
+    .cookies-notice .v-btn {
+      width: 100%;
+      margin-top: 16px;
+      margin-left: 0px !important;
+      margin-right: 0px !important;
+      margin-bottom: 0px;
+      min-height: 48px;
+    }
+  }
+
 </style>
 
 <script>
